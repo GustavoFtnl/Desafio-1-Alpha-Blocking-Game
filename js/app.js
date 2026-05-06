48f1c271d6800117b721e2227d976b128013a5c7
Gabriel Alves <jgabrielalves@alu.ufc.br>
2026-05-06 14:53:39 -0300

    feat: refactor app structure and add Game page implementation

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /**
  * app.js - Ponto de entrada principal da aplicação
- * Orquestra componentes, eventos e execução
+ * Orquestra entre páginas Home e Game
  * Comentários em português do Brasil
  */
 
@@ -9,14 +9,7 @@ import { gameState } from "./state.js";
 import DOM from "./dom.js";
 
 import { Home } from "./pages/Home.js";
-import { TopBar } from "./components/TopBar.js";
-import { Sidebar } from "./components/Sidebar.js";
-import { Workspace } from "./components/Workspace.js";
-import { Stage } from "./components/Stage.js";
-import { DragDrop } from "./components/DragDrop.js";
-import { Modal } from "./components/Modal.js";
-import { Parser } from "./engine/parser.js";
-import { Runner } from "./engine/runner.js";
+import { Game } from "./pages/Game.js";
 
 var App = function() {
   this.mode = "home";
@@ -40,18 +33,12 @@ App.prototype.setupGlobalListeners = function() {
   document.addEventListener("exitToHome", function() {
     self.exitToHome();
   });
-
-  gameState.addListener(function(event, data) {
-    if (event === "userNameChanged") {
-      self.updateUserNameDisplay();
-    }
-  });
 };
 
 App.prototype.checkMode = function() {
-  var hasName = gameState.getUserName() && gameState.getUserName().length > 0;
+  var hasCurrentUser = gameState.getUserName() && gameState.getUserName().length > 0;
   
-  if (hasName) {
+  if (hasCurrentUser) {
     this.showGameScreen();
   } else {
     this.showHomeScreen();
@@ -70,177 +57,17 @@ App.prototype.showHomeScreen = function() {
 
 App.prototype.showGameScreen = function() {
   this.mode = "game";
-  DOM.renderAppLayout();
+  var root = document.getElementById(CONFIG.DOM_IDS.ROOT);
+  root.innerHTML = "<div class=\"gameScreen\"></div>";
   
-  this.initComponents();
-  this.setupGameListeners();
-  DOM.updateUIFromState();
-};
-
-App.prototype.initComponents = function() {
-  var topBarContainer = DOM.getTopBarContainer();
-  var sidebarContainer = DOM.getSidebarContainer();
-  var workspaceContainer = DOM.getWorkspaceContainer();
-  var stageContainer = DOM.getStageContainer();
-
-  this.topBar = new TopBar(topBarContainer);
-  this.sidebar = new Sidebar(sidebarContainer);
-  this.workspace = new Workspace(workspaceContainer);
-  this.stage = new Stage(stageContainer);
-  this.modal = new Modal();
-  this.parser = new Parser();
-  this.runner = new Runner(this.stage);
-
-  this.dragDrop = new DragDrop(
-    this.sidebar.getPaletteElement(),
-    this.workspace.getWorkspaceElement()
-  );
-
-  topBarContainer.componentInstance = this.topBar;
-  sidebarContainer.componentInstance = this.sidebar;
-  workspaceContainer.componentInstance = this.workspace;
-  stageContainer.componentInstance = this.stage;
-};
-
-App.prototype.setupGameListeners = function() {
-  var self = this;
-  var workspaceContainer = DOM.getWorkspaceContainer();
-
-  workspaceContainer.addEventListener("blockCountChanged", function(e) {
-    var maxBlocks = gameState.getMaxBlocks();
-    self.stage.updateBlockCounter(e.detail.count);
-  });
-
-  var stageContainer = DOM.getStageContainer();
-  stageContainer.addEventListener("stageRun", function() { self.runCode(); });
-  stageContainer.addEventListener("stagePause", function() { self.togglePause(); });
-  stageContainer.addEventListener("stageClear", function() { self.clearWorkspace(); });
-
-  document.addEventListener("levelComplete", function(e) {
-    if (e.detail.success) {
-      self.handleLevelComplete();
-    }
-  });
-};
-
-App.prototype.updateUserNameDisplay = function() {
-  if (this.topBar && this.topBar.updateUserName) {
-    this.topBar.updateUserName(gameState.getUserName());
-  }
-};
-
-App.prototype.runCode = function() {
-  var self = this;
-  var totalBlocks = this.parser.countBlocks();
-  var maxBlocks = gameState.getMaxBlocks();
-
-  if (totalBlocks > maxBlocks) {
-    alert("Você excedeu o limite de blocos! Máximo: " + maxBlocks + ", usados: " + totalBlocks + ".");
-  }
-
-  if (this.runner.running) {
-    this.runner.stop();
-    setTimeout(function() {
-      self.executeInstructions();
-    }, 100);
-  } else {
-    this.executeInstructions();
-  }
-};
-
-App.prototype.executeInstructions = function() {
-  var instructions = this.parser.parse();
-
-  if (instructions.length === 0) {
-    alert("Adicione blocos ao workspace antes de executar!");
-    return;
-  }
-
-  this.stage.reset();
-  this.runner.run(instructions);
-};
-
-App.prototype.togglePause = function() {
-  if (this.runner.paused) {
-    this.runner.resume();
-  } else {
-    this.runner.pause();
-  }
-};
-
-App.prototype.clearWorkspace = function() {
-  if (this.runner.running) {
-    this.runner.stop();
-  }
-
-  this.workspace.clear();
-  this.stage.reset();
-};
-
-App.prototype.handleLevelComplete = function() {
-  var totalBlocks = this.parser.countBlocks();
-  var maxBlocks = gameState.getMaxBlocks();
-
-  var stars = gameState.completeLevel(totalBlocks);
-
-  if (gameState.getCurrentLevel() >= gameState.getTotalLevels()) {
-    this.showGameCompleteModal(stars);
-  } else {
-    this.showLevelCompleteModal(stars, maxBlocks, totalBlocks);
-  }
-};
-
-App.prototype.showLevelCompleteModal = function(stars, maxBlocks, usedBlocks) {
-  var self = this;
-  var contentHtml = Modal.createLevelCompleteHtml(stars, maxBlocks, usedBlocks);
-
-  this.modal.open(contentHtml).then(function() {
-    var nextLevelBtn = self.modal.modalElement.querySelector("#nextLevelBtn");
-    if (nextLevelBtn) {
-      nextLevelBtn.addEventListener("click", function() {
-        self.modal.close();
-        gameState.advanceLevel();
-        self.clearWorkspace();
-      });
-    }
-  });
-};
-
-App.prototype.showGameCompleteModal = function(finalStars) {
-  var self = this;
-  var contentHtml = Modal.createGameCompleteHtml(finalStars);
-
-  this.modal.open(contentHtml).then(function() {
-    var restartBtn = self.modal.modalElement.querySelector("#restartCareerBtn");
-    if (restartBtn) {
-      restartBtn.addEventListener("click", function() {
-        self.modal.close();
-        self.restartCareer();
-      });
-    }
-  });
-};
-
-App.prototype.restartCareer = function() {
-  gameState.resetCareer();
-  DOM.clearWorkspaceVisual();
-  this.clearWorkspace();
+  var gameContainer = root.querySelector(".gameScreen");
+  this.game = new Game(gameContainer);
 };
 
 App.prototype.exitToHome = function() {
   if (confirm("Tem certeza que deseja sair? Seu progresso será salvo.")) {
-    if (this.runner && this.runner.running) {
-      this.runner.stop();
-    }
     this.showHomeScreen();
   }
-};
-
-App.prototype.updateUIFromState = function() {
-  this.topBar.updateLevel(gameState.getCurrentLevel(), gameState.getTotalLevels());
-  this.topBar.updateStars(gameState.getStarsForLevel(gameState.getCurrentLevel()));
-  this.topBar.updateProgress(gameState.getProgressPercent());
-  this.stage.setMaxBlocks(gameState.getMaxBlocks());
 };
 
 document.addEventListener("DOMContentLoaded", function() {
---
+++ js/pages/Game.js
@@ -0,0 +1,183 @@
+/**
+ * Game.js - Página principal do jogo
+ * Renderiza o layout do jogo (Sidebar, Workspace, Stage) e gerencia execução
+ * Comentários em português do Brasil
+ */
+
+import { gameState } from "../state.js";
+import DOM from "../dom.js";
+
+import { TopBar } from "../components/TopBar.js";
+import { Sidebar } from "../components/Sidebar.js";
+import { Workspace } from "../components/Workspace.js";
+import { Stage } from "../components/Stage.js";
+import { DragDrop } from "../components/DragDrop.js";
+import { Modal } from "../components/Modal.js";
+import { Parser } from "../engine/parser.js";
+import { Runner } from "../engine/runner.js";
+
+var Game = function(container) {
+  this.container = container;
+  this.render();
+};
+
+Game.prototype.render = function() {
+  DOM.renderAppLayout();
+  this.initComponents();
+  this.setupListeners();
+  DOM.updateUIFromState();
+};
+
+Game.prototype.initComponents = function() {
+  var topBarContainer = DOM.getTopBarContainer();
+  var sidebarContainer = DOM.getSidebarContainer();
+  var workspaceContainer = DOM.getWorkspaceContainer();
+  var stageContainer = DOM.getStageContainer();
+
+  this.topBar = new TopBar(topBarContainer);
+  this.sidebar = new Sidebar(sidebarContainer);
+  this.workspace = new Workspace(workspaceContainer);
+  this.stage = new Stage(stageContainer);
+  this.modal = new Modal();
+  this.parser = new Parser();
+  this.runner = new Runner(this.stage);
+
+  this.dragDrop = new DragDrop(
+    this.sidebar.getPaletteElement(),
+    this.workspace.getWorkspaceElement()
+  );
+
+  topBarContainer.componentInstance = this.topBar;
+  sidebarContainer.componentInstance = this.sidebar;
+  workspaceContainer.componentInstance = this.workspace;
+  stageContainer.componentInstance = this.stage;
+};
+
+Game.prototype.setupListeners = function() {
+  var self = this;
+  var workspaceContainer = DOM.getWorkspaceContainer();
+
+  workspaceContainer.addEventListener("blockCountChanged", function(e) {
+    var maxBlocks = gameState.getMaxBlocks();
+    self.stage.updateBlockCounter(e.detail.count);
+  });
+
+  var stageContainer = DOM.getStageContainer();
+  stageContainer.addEventListener("stageRun", function() { self.runCode(); });
+  stageContainer.addEventListener("stagePause", function() { self.togglePause(); });
+  stageContainer.addEventListener("stageClear", function() { self.clearWorkspace(); });
+
+  document.addEventListener("levelComplete", function(e) {
+    if (e.detail.success) {
+      self.handleLevelComplete();
+    }
+  });
+};
+
+Game.prototype.runCode = function() {
+  var self = this;
+  var totalBlocks = this.parser.countBlocks();
+  var maxBlocks = gameState.getMaxBlocks();
+
+  if (totalBlocks > maxBlocks) {
+    alert("Você excedeu o limite de blocos! Máximo: " + maxBlocks + ", usados: " + totalBlocks + ".");
+  }
+
+  if (this.runner.running) {
+    this.runner.stop();
+    setTimeout(function() {
+      self.executeInstructions();
+    }, 100);
+  } else {
+    this.executeInstructions();
+  }
+};
+
+Game.prototype.executeInstructions = function() {
+  var instructions = this.parser.parse();
+
+  if (instructions.length === 0) {
+    alert("Adicione blocos ao workspace antes de executar!");
+    return;
+  }
+
+  this.stage.reset();
+  this.runner.run(instructions);
+};
+
+Game.prototype.togglePause = function() {
+  if (this.runner.paused) {
+    this.runner.resume();
+  } else {
+    this.runner.pause();
+  }
+};
+
+Game.prototype.clearWorkspace = function() {
+  if (this.runner.running) {
+    this.runner.stop();
+  }
+
+  this.workspace.clear();
+  this.stage.reset();
+};
+
+Game.prototype.handleLevelComplete = function() {
+  var totalBlocks = this.parser.countBlocks();
+  var maxBlocks = gameState.getMaxBlocks();
+
+  var stars = gameState.completeLevel(totalBlocks);
+
+  if (gameState.getCurrentLevel() >= gameState.getTotalLevels()) {
+    this.showGameCompleteModal(stars);
+  } else {
+    this.showLevelCompleteModal(stars, maxBlocks, totalBlocks);
+  }
+};
+
+Game.prototype.showLevelCompleteModal = function(stars, maxBlocks, usedBlocks) {
+  var self = this;
+  var contentHtml = Modal.createLevelCompleteHtml(stars, maxBlocks, usedBlocks);
+
+  this.modal.open(contentHtml).then(function() {
+    var nextLevelBtn = self.modal.modalElement.querySelector("#nextLevelBtn");
+    if (nextLevelBtn) {
+      nextLevelBtn.addEventListener("click", function() {
+        self.modal.close();
+        gameState.advanceLevel();
+        self.clearWorkspace();
+      });
+    }
+  });
+};
+
+Game.prototype.showGameCompleteModal = function(finalStars) {
+  var self = this;
+  var contentHtml = Modal.createGameCompleteHtml(finalStars);
+
+  this.modal.open(contentHtml).then(function() {
+    var restartBtn = self.modal.modalElement.querySelector("#restartCareerBtn");
+    if (restartBtn) {
+      restartBtn.addEventListener("click", function() {
+        self.modal.close();
+        self.restartCareer();
+      });
+    }
+  });
+};
+
+Game.prototype.restartCareer = function() {
+  gameState.resetCareer();
+  DOM.clearWorkspaceVisual();
+  this.clearWorkspace();
+};
+
+Game.prototype.updateUI = function() {
+  this.topBar.updateLevel(gameState.getCurrentLevel(), gameState.getTotalLevels());
+  this.topBar.updateStars(gameState.getStarsForLevel(gameState.getCurrentLevel()));
+  this.topBar.updateProgress(gameState.getProgressPercent());
+  this.stage.setMaxBlocks(gameState.getMaxBlocks());
+};
+
+export default Game;
+export { Game };
