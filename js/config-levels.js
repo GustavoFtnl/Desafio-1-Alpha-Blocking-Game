6a8f7cef6c6ff395538168635d214b8911d1e85e
Gustavo Fontenele <gustavofonteneledev@gmail.com>
2026-05-07 22:17:52 -0300

    feat: enhance level 3 and level 4 layouts

--- js/config-levels.js
+++ js/config-levels.js
@@ -221,33 +221,215 @@ const CONFIG_LEVELS = [
   // Nível 3: Primeira Armadilha
   {
     id: 3,
-    name: "Primeira Armadilha",
-    start: { x: 0, y: 0 },
-    trophy: { x: 3, y: 0 },
-    walls: [
+    name: "Campo Explosivo",
+
+    start: { x: 7, y: 0 },
+    trophy: { x: 5, y: 7 },
+
+    holes: [
       { x: 1, y: 0 },
       { x: 2, y: 0 },
+
+      { x: 2, y: 2 },
+
+      { x: 9, y: 3 },
+      { x: 9, y: 4 },
+      { x: 9, y: 5 },
+      { x: 9, y: 6 },
+
+      { x: 3, y: 6 },
+
+      { x: 2, y: 8 },
     ],
-    traps: [
-      { x: 1, y: 1 },
+
+    walls: [
+      // Coluna esquerda
+      { x: 0, y: 0 },
       { x: 0, y: 1 },
+      { x: 0, y: 2 },
+      { x: 0, y: 3 },
+      { x: 0, y: 4 },
+      { x: 0, y: 5 },
+      { x: 0, y: 6 },
+      { x: 0, y: 7 },
+      { x: 0, y: 8 },
+      { x: 0, y: 9 },
+
+      // Parte superior
+      { x: 6, y: 0 },
+      { x: 8, y: 0 },
+
+      // Linha 1
+      { x: 6, y: 1 },
+      { x: 8, y: 1 },
+
+      // Linha 2
+      { x: 8, y: 2 },
+
+      // Linha 3
+      { x: 3, y: 3 },
+      { x: 4, y: 3 },
+      { x: 5, y: 3 },
+      { x: 6, y: 3 },
+      { x: 7, y: 3 },
+      { x: 8, y: 3 },
+
+      // Linha 4
+      { x: 3, y: 4 },
+      { x: 5, y: 4 },
+      { x: 6, y: 4 },
+      { x: 7, y: 4 },
+      { x: 8, y: 4 },
+
+      // Linha 5
+      { x: 3, y: 5 },
+      { x: 8, y: 5 },
+
+      // Linha 6
+      { x: 5, y: 6 },
+      { x: 6, y: 6 },
+      { x: 8, y: 6 },
+
+      // Linha 7
+      { x: 3, y: 7 },
+      { x: 6, y: 7 },
+      { x: 8, y: 7 },
+
+      // Linha 8
+      { x: 3, y: 8 },
+      { x: 4, y: 8 },
+      { x: 5, y: 8 },
+      { x: 6, y: 8 },
+      { x: 8, y: 8 },
+
+      // Linha 9
+      { x: 9, y: 9 },
     ],
-    maxBlocks: 12,
+
+    traps: [
+      { x: 3, y: 0 },
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+
+      { x: 2, y: 1 },
+      { x: 4, y: 1 },
+
+      { x: 3, y: 2 },
+      { x: 4, y: 2 },
+
+      { x: 4, y: 4 },
+    ],
+
+    maxBlocks: 24,
   },
 
   // Nível 4: Desvio Simples
   {
     id: 4,
-    name: "Desvio Simples",
-    start: { x: 0, y: 0 },
-    trophy: { x: 4, y: 1 },
+    name: "Fortaleza Explosiva",
+
+    start: { x: 1, y: 0 },
+    trophy: { x: 9, y: 7 },
+
+    holes: [],
+
     walls: [
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
+      // Linha 0
       { x: 3, y: 0 },
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 6, y: 0 },
+      { x: 7, y: 0 },
+      { x: 8, y: 0 },
+      { x: 9, y: 0 },
+
+      // Linha 1
+      { x: 3, y: 1 },
+      { x: 4, y: 1 },
+      { x: 5, y: 1 },
+      { x: 6, y: 1 },
+      { x: 7, y: 1 },
+      { x: 8, y: 1 },
+      { x: 9, y: 1 },
+
+      // Linha 2
+      { x: 3, y: 2 },
+      { x: 4, y: 2 },
+      { x: 5, y: 2 },
+      { x: 6, y: 2 },
+      { x: 7, y: 2 },
+      { x: 8, y: 2 },
+      { x: 9, y: 2 },
+
+      // Linha 3
+      { x: 3, y: 3 },
+      { x: 4, y: 3 },
+      { x: 5, y: 3 },
+      { x: 6, y: 3 },
+      { x: 7, y: 3 },
+      { x: 8, y: 3 },
+      { x: 9, y: 3 },
+
+      // Linha 4
+      { x: 3, y: 4 },
+      { x: 4, y: 4 },
+      { x: 5, y: 4 },
+      { x: 6, y: 4 },
+      { x: 7, y: 4 },
+      { x: 8, y: 4 },
+      { x: 9, y: 4 },
+
+      // Linha 5
+      { x: 3, y: 5 },
+      { x: 4, y: 5 },
+      { x: 5, y: 5 },
+      { x: 6, y: 5 },
+      { x: 7, y: 5 },
+      { x: 8, y: 5 },
+      { x: 9, y: 5 },
+
+      // Linha inferior
+      { x: 0, y: 9 },
+      { x: 1, y: 9 },
+      { x: 2, y: 9 },
+      { x: 3, y: 9 },
+      { x: 4, y: 9 },
+      { x: 5, y: 9 },
+      { x: 6, y: 9 },
+      { x: 7, y: 9 },
+      { x: 8, y: 9 },
+      { x: 9, y: 9 },
     ],
-    traps: [],
-    maxBlocks: 14,
+
+    traps: [
+      // Parte superior esquerda
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+
+      { x: 0, y: 3 },
+      { x: 1, y: 3 },
+      { x: 2, y: 3 },
+
+      { x: 0, y: 5 },
+      { x: 1, y: 5 },
+      { x: 2, y: 5 },
+
+      // Parte inferior
+      { x: 2, y: 6 },
+      { x: 4, y: 6 },
+      { x: 7, y: 6 },
+
+      { x: 2, y: 7 },
+      { x: 4, y: 7 },
+      { x: 7, y: 7 },
+
+      { x: 2, y: 8 },
+      { x: 4, y: 8 },
+      { x: 7, y: 8 },
+    ],
+
+    maxBlocks: 30,
   },
 
   // Nível 5: Armadilha e Parede
