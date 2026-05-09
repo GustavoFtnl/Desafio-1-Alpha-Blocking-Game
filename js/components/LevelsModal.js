72bc39b945028ee1fd1d9f7676bd56b58f5f5675
Gustavo Fontenele <gustavofonteneledev@gmail.com>
2026-05-08 22:17:45 -0300

    refactor: update star size in LevelsModal

--- js/components/LevelsModal.js
+++ js/components/LevelsModal.js
@@ -117,7 +117,7 @@ export class LevelsModal {
     for (let i = 0; i < 3; i++) {
       const color = i < starCount ? filledColor : emptyColor;
       html += `
-        <svg class="starSvg" viewBox="0 0 24 24" width="20" height="20">
+        <svg class="starSvg" viewBox="0 0 24 24" width="36" height="36">
           <path fill="${color}" d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/>
         </svg>
       `;
